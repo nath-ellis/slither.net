f713b7bb54e6a06c26382e0221fcba416c036d93
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-03-31 14:40:57 +0100

    Added a way to toggle a speed boost, decreasing the player's length as it is used

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -4,8 +4,4 @@ extends Node
 var player_speed = 64
 var player_vel = Vector2(player_speed, 0)
 var player_pos = Vector2()
-var length = 0
-
-
-func increase_length() -> void:
-	length += 1
+var length = 2
