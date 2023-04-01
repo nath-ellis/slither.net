a1c122b6ba2ccc58c99456f30a4fb14d72763a51
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-04-01 10:33:37 +0100

    Added larger bits which increase size much more

--- scripts/bit.gd
+++ scripts/bit.gd
@@ -12,6 +12,9 @@ func _on_area_entered(area) -> void:
 	Manager.length += 1
 	
 	if area.get_parent().has_method("increase_length"):
-		area.get_parent().call_deferred("increase_length", 3)
+		if get_meta("size") == "large":
+			area.get_parent().call_deferred("increase_length", 10)
+		elif get_meta("size") == "small":
+			area.get_parent().call_deferred("increase_length", 2)
 	
 	queue_free()
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 const BIT = preload("res://scenes/bit.tscn")
+const LARGE_BIT = preload("res://scenes/large_bit.tscn")
 
 @onready var player = $Player
 @onready var bits = $Bits
@@ -12,7 +13,11 @@ func _on_bit_timer_timeout() -> void:
 	var chance = randi_range(0, 15)
 	
 	if chance == 1:
-		var new_bit = BIT.instantiate()
-		bits.add_child(new_bit)
+		bits.add_child(BIT.instantiate())
+	else:
+		chance = randi_range(0, 30)
+		
+		if chance == 1:
+			bits.add_child(LARGE_BIT.instantiate())
 	
 	bit_timer.start()
