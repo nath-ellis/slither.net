b3c31ef1274e34d2da8333c8e25fe3eff3b0d2e7
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-04-01 11:02:53 +0100

    Added different coloured sprites for bits

--- scripts/bit.gd
+++ scripts/bit.gd
@@ -1,11 +1,48 @@
 extends Area2D
 
 
+@onready var sprite = $Sprite2D
+
+
 func _ready() -> void:
 	global_position = Vector2(
 		randi_range(Manager.player_pos.x - get_viewport_rect().size.x, Manager.player_pos.x + get_viewport_rect().size.x),
 		randi_range(Manager.player_pos.y - get_viewport_rect().size.y, Manager.player_pos.y + get_viewport_rect().size.y)
 	)
+	
+	if get_meta("size") == "large":
+		match randi_range(1, 7):
+			1:
+				sprite.texture = load("res://assets/bits/large_blue.png")
+			2:
+				sprite.texture = load("res://assets/bits/large_green.png")
+			3:
+				sprite.texture = load("res://assets/bits/large_light_blue.png")
+			4:
+				sprite.texture = load("res://assets/bits/large_lime.png")
+			5:
+				sprite.texture = load("res://assets/bits/large_orange.png")
+			6:
+				sprite.texture = load("res://assets/bits/large_white.png")
+			7:
+				sprite.texture = load("res://assets/bits/large_yellow.png")
+				
+	elif get_meta("size") == "small":
+		match randi_range(1, 7):
+			1:
+				sprite.texture = load("res://assets/bits/blue.png")
+			2:
+				sprite.texture = load("res://assets/bits/green.png")
+			3:
+				sprite.texture = load("res://assets/bits/light_blue.png")
+			4:
+				sprite.texture = load("res://assets/bits/lime.png")
+			5:
+				sprite.texture = load("res://assets/bits/orange.png")
+			6:
+				sprite.texture = load("res://assets/bits/white.png")
+			7:
+				sprite.texture = load("res://assets/bits/yellow.png")
 
 
 func _on_area_entered(area) -> void:
@@ -14,6 +51,7 @@ func _on_area_entered(area) -> void:
 	if area.get_parent().has_method("increase_length"):
 		if get_meta("size") == "large":
 			area.get_parent().call_deferred("increase_length", 10)
+			
 		elif get_meta("size") == "small":
 			area.get_parent().call_deferred("increase_length", 2)
 	
