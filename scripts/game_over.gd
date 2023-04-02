538b1d44b7daf71b31295f9a4fc7fcc6cbd7ddaf
Nathan <nathanellis15782@protonmail.com>
2023-04-02 13:04:26 +0100

    Added a game over screen

---
+++ scripts/game_over.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+func _on_play_again_pressed() -> void:
+	Manager.player_vel = Vector2(Manager.player_speed, 0)
+	Manager.length = 2
+	
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
