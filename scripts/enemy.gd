c1dc596c1d13fd85491e360aad9a6b59fadc5d80
Nathan <nathanellis15782@protonmail.com>
2023-04-03 08:44:28 +0100

    Added enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 
-const PLAYER_BODY = preload("res://scenes/player_body.tscn")
+const ENEMY_BODY = preload("res://scenes/enemy_body.tscn")
 
+var vel = Vector2(Manager.player_speed, 0)
 var moved = false
 var sped_up = false
 var increase_length_counter = 0
 var lose_length_counter = 0
 
-@onready var face = $Face
-@onready var face_sprite = $Face/Sprite
+@onready var face = $EnemyFace
+@onready var face_sprite = $EnemyFace/Sprite
 @onready var body = $Body
 @onready var movement_timer = $MovementTimer
 
@@ -17,18 +18,18 @@ var lose_length_counter = 0
 func add_move_to() -> void:
 	for b in body.get_children():
 		# Don't add duplicates
-		if !b.move_to.has({"x": face.global_position.x, "y": face.global_position.y, "vel": Manager.player_vel}):
+		if !b.move_to.has({"x": face.global_position.x, "y": face.global_position.y, "vel": vel}):
 			b.move_to.append({
 				"x": face.global_position.x,
 				"y": face.global_position.y,
-				"vel": Manager.player_vel,
+				"vel": vel,
 			})
 
 
 func _process(_delta) -> void:
 	# Check whether the player's size should be increased
 	if increase_length_counter >= 10:
-		var new_body = PLAYER_BODY.instantiate()
+		var new_body = ENEMY_BODY.instantiate()
 		var end_body = body.get_children()[len(body.get_children())-1]
 		
 		if len(end_body.move_to) > 0:
@@ -39,59 +40,62 @@ func _process(_delta) -> void:
 				new_body.move_to.append(e)
 			
 		else:
-			new_body.position = end_body.position - Manager.player_vel
+			new_body.position = end_body.position - vel
 		
 		body.add_child(new_body)
 		
-		Manager.length += 1
 		increase_length_counter -= 10
+
 
 func _physics_process(_delta) -> void:
 	# Prevent the player from changing direction too fast
 	if !moved:
-		if Input.is_action_pressed("up") and Manager.player_vel.y == 0:
+		var chance = randi_range(1, 100)
+		
+		if chance == 1 and vel.y == 0:
 			add_move_to()
-			Manager.player_vel = Vector2(0, -Manager.player_speed)
+			vel = Vector2(0, -Manager.player_speed)
 			moved = true
 			
-		elif Input.is_action_pressed("down") and Manager.player_vel.y == 0:
+		elif chance == 2 and vel.y == 0:
 			add_move_to()
-			Manager.player_vel = Vector2(0, Manager.player_speed)
+			vel = Vector2(0, Manager.player_speed)
 			moved = true
 			
-		elif Input.is_action_pressed("left") and Manager.player_vel.x == 0:
+		elif chance == 3 and vel.x == 0:
 			add_move_to()
-			Manager.player_vel = Vector2(-Manager.player_speed, 0)
+			vel = Vector2(-Manager.player_speed, 0)
 			moved = true
 			
-		elif Input.is_action_pressed("right") and Manager.player_vel.x == 0:
+		elif chance == 4 and vel.x == 0:
 			add_move_to()
-			Manager.player_vel = Vector2(Manager.player_speed, 0)
+			vel = Vector2(Manager.player_speed, 0)
 			moved = true
 	
-	if Input.is_action_just_pressed("speed_up") and !sped_up and body.get_child_count() > 1:
+	var chance = randi_range(1, 100)
+	
+	if chance == 1 and !sped_up and body.get_child_count() > 1:
 		sped_up = true
 	
-	if Input.is_action_just_released("speed_up") and sped_up:
+	if chance % 8 == 0 and sped_up:
 		sped_up = false
 
 
 func _on_movement_timer_timeout() -> void:
 	# Rotate sprite on movement to prevent glitchiness
-	if Manager.player_vel.y < 0:
+	if vel.y < 0:
 		face_sprite.rotation_degrees = 0
 		
-	elif Manager.player_vel.y > 0:
+	elif vel.y > 0:
 		face_sprite.rotation_degrees = 180
 		
-	elif Manager.player_vel.x < 0:
+	elif vel.x < 0:
 		face_sprite.rotation_degrees = 270
 		
-	elif Manager.player_vel.x > 0:
+	elif vel.x > 0:
 		face_sprite.rotation_degrees = 90
 	
-	face.global_position = face.global_position + Manager.player_vel
-	Manager.player_pos = face.global_position
+	face.global_position = face.global_position + vel
 	
 	for b in body.get_children():
 		b.call("move")
@@ -123,6 +127,9 @@ func increase_length(increase_amount) -> void:
 	increase_length_counter += increase_amount
 
 
-func _on_face_area_entered(area):
-	if area.name == "EnemyFace" and area.global_position == global_position:
+func _on_enemy_face_area_entered(area):
+	if area.name == "Face":
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+	
+	if area.name == "EnemyFace":
 		area.get_parent().queue_free()
