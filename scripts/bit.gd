846e85d18e586af071f8e5a5ce312cccb48744bb
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-04-04 16:43:04 +0100

    Added a timer which removes the bit after a random amount of time

--- scripts/bit.gd
+++ scripts/bit.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 @onready var sprite = $Sprite2D
+@onready var timer = $Timer
 
 
 func _ready() -> void:
@@ -9,6 +10,10 @@ func _ready() -> void:
 		randi_range(Manager.player_pos.x - get_viewport_rect().size.x, Manager.player_pos.x + get_viewport_rect().size.x),
 		randi_range(Manager.player_pos.y - get_viewport_rect().size.y, Manager.player_pos.y + get_viewport_rect().size.y)
 	)
+	
+	# Time that the bit will be in the game for
+	timer.wait_time = randf_range(10.0, 100.0)
+	timer.start()
 	
 	if get_meta("size") == "large":
 		match randi_range(1, 7):
@@ -53,4 +58,8 @@ func _on_area_entered(area) -> void:
 		elif get_meta("size") == "small":
 			area.get_parent().call_deferred("increase_length", 2)
 	
+	queue_free()
+
+
+func _on_timer_timeout():
 	queue_free()
