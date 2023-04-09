331f13357f5e15fa65364786a7f71a33dc81daec
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-04-09 09:54:13 +0100

    Added a leaderboard

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,6 +5,14 @@ const BIT = preload("res://scenes/bit.tscn")
 const LARGE_BIT = preload("res://scenes/large_bit.tscn")
 const ENEMY = preload("res://scenes/enemy.tscn")
 
+var leaderboad_content = [
+	["name", 10],
+	["name", 8],
+	["name", 6],
+	["name", 5],
+	["name", 4],
+]
+
 @onready var player = $Player
 @onready var bits = $Bits
 @onready var bit_timer = $BitTimer
@@ -13,6 +21,14 @@ const ENEMY = preload("res://scenes/enemy.tscn")
 @onready var length = $UI/Control/Length
 @onready var enemies = $Enemies
 @onready var enemy_timer = $EnemyTimer
+@onready var leaderboard = $UI/Control/Leaderboard
+@onready var leaderboad_labels = [
+	$UI/Control/Leaderboard/First,
+	$UI/Control/Leaderboard/Second,
+	$UI/Control/Leaderboard/Third,
+	$UI/Control/Leaderboard/Fourth,
+	$UI/Control/Leaderboard/Fifth,
+]
 
 
 func _process(_delta) -> void:
@@ -24,6 +40,8 @@ func _process(_delta) -> void:
 			growth_progress_bar.value = player.increase_length_counter * 10
 	
 	length.text = str(Manager.length)
+	
+	update_leaderboard()
 
 
 func _on_bit_timer_timeout() -> void:
@@ -46,6 +64,56 @@ func _on_growth_progress_bar_value_changed(value) -> void:
 		growth_particles.emitting = true
 
 
-func _on_enemy_timer_timeout():
+func _on_enemy_timer_timeout() -> void:
 	enemies.add_child(ENEMY.instantiate())
 	enemy_timer.start()
+
+
+func update_leaderboard() -> void:
+	leaderboad_content = []  # Clear
+	
+	# Add filler data so that next code can run
+	for e in enemies.get_children():
+		leaderboad_content.append([e.username.text, e.length])
+	
+	# Add correct data to the leaderboad
+	for e in enemies.get_children():
+		var insert_at = -1
+		
+		if e.length >= leaderboad_content[0][1]:
+			insert_at = 0
+		elif e.length >= leaderboad_content[1][1]:
+			insert_at = 1
+		elif e.length >= leaderboad_content[2][1]:
+			insert_at = 2
+		elif e.length >= leaderboad_content[3][1]:
+			insert_at = 3
+		elif e.length >= leaderboad_content[4][1]:
+			insert_at = 4
+		
+		if insert_at != -1:
+			leaderboad_content.insert(insert_at, [e.username.text, e.length])
+	
+	# Add player if needed
+	var insert_at = -1
+	
+	if Manager.length >= leaderboad_content[0][1]:
+		insert_at = 0
+	elif Manager.length >= leaderboad_content[1][1]:
+		insert_at = 1
+	elif Manager.length >= leaderboad_content[2][1]:
+		insert_at = 2
+	elif Manager.length >= leaderboad_content[3][1]:
+		insert_at = 3
+	elif Manager.length >= leaderboad_content[4][1]:
+		insert_at = 4
+	
+	if insert_at != -1:
+		leaderboad_content.insert(insert_at, [Manager.player_name, Manager.length])
+	
+	# Remove data past index 4
+	while len(leaderboad_content) > 5:
+		leaderboad_content.remove_at(5)
+	
+	for i in range(5):
+		leaderboad_labels[i].text = leaderboad_content[i][0] + ": " + str(leaderboad_content[i][1])
