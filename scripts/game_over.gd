4e81cec7d9490733a9bd9b7679c07e933468de6b
Nathan <101135961+nath-ellis@users.noreply.github.com>
2023-04-11 11:30:33 +0100

    Improved the game over scene

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,14 +1,67 @@
 extends Node
 
 
+@onready var length_label = $UI/Length
+@onready var snake = $UI/Snake
+@onready var face = $UI/Snake/Face
+@onready var body_1 = $UI/Snake/Body1
+
+
+func _ready() -> void:
+	"""
+	Adds the length that the player reached to the label.
+	Then changes sprites and length of the snake.
+	"""
+	
+	length_label.text += str(Manager.length)
+	
+	var body_1_sprite = load("res://assets/snakes/" + Manager.player_colour + "/body_1.png")
+	var body_2_sprite = load("res://assets/snakes/" + Manager.player_colour + "/body_2.png")
+	
+	face.texture = load("res://assets/snakes/" + Manager.player_colour + "/dead.png")
+	body_1.texture = body_2_sprite
+	
+	for i in range(Manager.length - 1):
+		var new_body = Sprite2D.new()
+		
+		# Alternates the sprites
+		if i % 2 == 0 or i == 0:
+			new_body.texture = body_1_sprite
+			
+			if snake.position.x < 1248:
+				snake.position.x += 64  # Ensures snake is centred
+		else:
+			new_body.texture = body_2_sprite
+		
+		new_body.position = snake.get_children()[len(snake.get_children())-1].position
+		new_body.position.x -= 64
+		
+		snake.add_child(new_body)
+
+
 func _on_play_again_pressed() -> void:
 	"""
-	Resets Manager.player_pos, Manager.player_vel and Manager.length and changes
-	the scene back to game.tscn.
+	Changes the scene back to game.tscn.
+	"""
+	
+	reset()
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_menu_pressed():
+	"""
+	Changes the scene to menu.tscn.
+	"""
+	
+	reset()
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func reset() -> void:
+	"""
+	Resets Manager.player_pos, Manager.player_vel and Manager.length.
 	"""
 	
 	Manager.player_pos = Vector2()
 	Manager.player_vel = Vector2(Manager.player_speed, 0)
 	Manager.length = 2
-	
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
